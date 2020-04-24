b590966989fe6a7c17f2a7a5bc24ffce8dc9d221
unknown <aim126@yahoo.com>
2020-04-24 08:07:55 -0500

    seeding Roles in the Database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace TrashCollectorProject.Data
 {
     public class ApplicationDbContext : IdentityDbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        {
+
+        public
+    ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                : base(options)
         {
         }
-    }
+        protected override void OnModeCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IndentityRole>()
+                 .HasData(
+                     new  IdentityRole
+                     {
+                         Name = "Admin",
+                         NormalizedName = "ADMIN"
+                     }
+                 );
+        }
+    } 
 }
