961ddebe426cdfff6c71ae0c69406de5c1796158
unknown <aim126@yahoo.com>
2020-04-24 17:00:44 -0500

    setting up config services

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -12,17 +12,23 @@ namespace TrashCollectorProject.Models
 {
     public class Customer
     {
-      [Key]
-      public int Id { get; set; }
-      public string Name { get; set; }
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
 
-      [ForeignKey("IndentityUser")]
-      public string IdeneityUserId { get; set; }
-      public IdentityUser IdentityUser { get; set; }
-
-        
-      
+        [ForeignKey("IndentityUser")]
+        public string IdeneityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
     }
+     // var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+     // customer.IdentityUserId = userId;
+     // _context.Add(customer);
+     // _context.SaveChanges();
+
+     //var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+     //   var customer = _context.Customers.Where(c => c.IdentityUserId ==
+     //   userId).SingleOrDefault();
+    
     
     
 }
--- Startup.cs
+++ Startup.cs
@@ -20,6 +20,8 @@ namespace TrashCollectorProject
 {
     public class Startup
     {
+        private readonly bool config;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -34,45 +36,54 @@ namespace TrashCollectorProject
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
-           
+
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
-
-
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddScoped<ClaimsPrincipal>(s =>
+                  s.GetService < IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config =>
+            {
+                config.Filters.Add(typeof(GlobalRouting));
+
+            });
+
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                    app.UseDatabaseErrorPage();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+
+                app.UseAuthentication();
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Home}/{action=Index}/{id?}");
+                    endpoints.MapRazorPages();
+                });
+            }
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        private class GlobalRouting
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
         }
     }
 }
